3e3a6c89bdbc61baf36e281be8bd553d0c685345
TU7SHAR <brahmin00011@gmail.com>
2026-05-12 16:13:03 +0530

    cmd file updated

--- .cmd
+++ .cmd
@@ -1,5 +1,14 @@
 @REM This file contains commands for running specific tasks 
 
+@REM for linnux virtual env
+python -m venv venv
+source venv/bin/activate
+
+@REM for windows virtual env
+python -m venv venv
+.\venv\Scripts\Activate.ps1
+
+
 pip install -r requirements.txt
 python app.py 
 python -c "import bcrypt; print(bcrypt.hashpw('YourPasswordHere'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'))"
