8387d446abd5a8dfd87d83b58b889f71b369ca9b
TU7SHAR <brahmin00011@gmail.com>
2026-06-16 11:25:24 +0530

    url

--- extensions.py
+++ extensions.py
@@ -9,5 +9,5 @@ from flask_limiter.util import get_remote_address
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["200 per hour"],
-    storage_uri="memory://",  # Switch to "redis://localhost:6379" when Redis is added
+    storage_uri="memory://",  
 )
